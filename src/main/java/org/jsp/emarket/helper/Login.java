261b1e55e7f4d12e793c8a19456a18af322178bb
Manoj_b_h <hhhmanu2@gmail.com>
2024-12-27 09:30:37 +0530

    first commit

---
+++ src/main/java/org/jsp/emarket/helper/Login.java
@@ -0,0 +1,21 @@
+package org.jsp.emarket.helper;
+
+import lombok.Data;
+
+@Data
+public class Login {
+	private String email;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	private String password;
+}
